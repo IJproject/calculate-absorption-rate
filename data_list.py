d5029a8e017c37990306fdea93a048ab118c4d57
IJproject <junchan3340@gmail.com>
2023-12-30 17:49:07 +0900

    ファイルの読み込み＋処理部分以外完了

---
+++ data_list.py
@@ -0,0 +1,4 @@
+data_list = [
+    {"material": "carbon_dioxide", "vertical_devisions": 100}, #二酸化炭素
+    {"material": "water_vapor", "vertical_devisions": 150}, #水蒸気
+]
--- main.py
+++ main.py
@@ -1,25 +1,47 @@
+import os
 import numpy as np
+from scipy.constants import pi, c, h, k
+from data_list import data_list
 
-from scipy.constants import pi
+whole_energy = 390.1046951106533 #全輻射エネルギー
+dir_path = '/absorption-spectrum-data/' #吸収スペクトルデータのディレクトリ
 
-# 積分条件
-number_of_divisions = 10000
-integral_start_value = 0 
-integral_finish_value = 20
+def calculate(data):
 
-wave_length = np.linspace(integral_start_value, integral_finish_value, number_of_divisions)
+    # プランクの熱輻射公式
+    def absorption_intensity(x):
+        return (2 * h * c**2 / x**5) * (1 / (np.exp((h * c) / (x * k * 288)) - 1))
 
-def absorption_intensity(x):
-    return x**2
+    #ファイルの読み込み
+    material = data['material']
+    # vertical_devisions = data['vertical_devisions']
+    # file_name = material + '.txt'
+    # with open(dir_path + file_name, 'r') as file:
+    #     print("ファイル読み込み")
 
-def absorption_rate(x):
-    return x
+    # 積分条件
+    number_of_divisions = 10000 #分割数
+    lambda_start = 1 * 10 ** (-6) #積分範囲の下限
+    lambda_finish = 2 * 10 ** (-5) #積分範囲の上限
 
-# Numpyを使用して積分計算
-amount_of_absorption = np.trapz(absorption_rate(wave_length) * absorption_intensity(wave_length), wave_length)
+    # 積分に用いる変数の準備
+    wave_length = np.linspace(lambda_start, lambda_finish, number_of_divisions) #各微小領域の波長の配列
+    dx = (lambda_finish - lambda_start) / number_of_divisions  #区間の幅
 
+    # 区分求積
+    amount_of_absorption = 0
+    for i in range(number_of_divisions - 1):
+        # 各微小領域の面積を計算して合計する
+        amount_of_absorption += pi * (absorption_intensity(wave_length[i]) + absorption_intensity(wave_length[i+1])) / 2 * dx
+
+    amount_of_absorption_rate = amount_of_absorption / whole_energy * 100
+
+    # 計算結果の出力
+    with open('result.txt', 'a') as file:
+        file.write('「' + material + '」の吸収エネルギー割合：' + str(amount_of_absorption_rate) + '%' + '\n')
+        file.write('「' + material + '」の地表への再放射割合：' + str(amount_of_absorption_rate / 2) + '%' + '\n\n')
 
 if __name__ == '__main__':
-    print('積分計算結果: ', amount_of_absorption)
-    print('πの値: ', pi)
-    print('PyCharm')
+
+    for data in data_list:
+        calculate(data)
