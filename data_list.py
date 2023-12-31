cb4e90d8b4341b3f1e687554e591b5e3c87ad5bf
IJproject <junchan3340@gmail.com>
2023-12-31 13:09:13 +0900

    完成

--- data_list.py
+++ data_list.py
@@ -1,8 +1,8 @@
 data_list = [
     {"material": "carbon_dioxide", "vertical_devisions": 191, "horizontal_devisions": 1317}, #二酸化炭素
-    {"material": "water_vapor", "vertical_devisions": 191, "horizontal_devisions": 1318}, #水蒸気
+    {"material": "water_vapor", "vertical_devisions": 190, "horizontal_devisions": 1318}, #水蒸気
     {"material": "ozone", "vertical_devisions": 191, "horizontal_devisions": 1321}, #オゾン
     {"material": "methane", "vertical_devisions": 192, "horizontal_devisions": 1320}, #メタン
-    {"material": "total", "vertical_devisions": 196, "horizontal_devisions": 1323}, #合計
+    {"material": "total", "vertical_devisions": 195, "horizontal_devisions": 1321}, #合計
 
 ]
